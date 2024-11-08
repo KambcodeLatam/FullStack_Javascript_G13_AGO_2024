140117820d943fbd18d28547625614d6132c19bf
Jessica Sandoval <jessi.sandoval93@gmail.com>
2024-11-07 21:17:46 -0500

    homework

--- MODULO_3/Clase-4/workclass/comparadores.js
+++ MODULO_3/Clase-4/workclass/comparadores.js
@@ -75,7 +75,7 @@ console.log({ result });
 console.log({ result2 });
 console.log({ result3 });
 
-// Operador de fusion Nula (??)
+// Operador de fusion Nula (??) (Nullish coalescing)
     // Si un valor es null o undefined podemos asignar un retorno de información
 let user;
 console.log( user ?? 'Usuario indefinido')
@@ -99,6 +99,32 @@ console.log({ finalPrice1 });
 // 3. En caso de que se cumpla la anterior condición vamos a evaluar si el rol cumple con el rol predenterminado
 // 4. En caso de que no se cumpla la anterior condición (2) retornar un mensaje de error
 
+
+let nombre = 'Ana';
+let contraseña = '123456';
+let rol = 'admin';
+
+const nombreAsignado = 'Ana';
+const contraseñaAsignada = '123456';
+const rolAsignado = 'admin2';
+
+if(nombre === nombreAsignado && contraseña === contraseñaAsignada){
+    if(rol === rolAsignado) {
+        console.log('Acceso permitido')
+    } else {
+        console.log('No tienes permisos')
+    }
+} else {
+    console.log('Usuario y/o contraseña incorrecta')
+}
+
+
+const resultadoAdministrador = (nombre === nombreAsignado && contraseña === contraseñaAsignada) 
+                                ? (rol === rolAsignado) ? 'Acceso permitido' : 'No tienes permisos'
+                                : 'Usuario y/o contraseña incorrecta'
+
+
+console.log({resultadoAdministrador})
 
 
 
