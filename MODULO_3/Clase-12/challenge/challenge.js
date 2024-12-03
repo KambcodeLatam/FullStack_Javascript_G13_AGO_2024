7ed9e18518fbe236d443376ecf5ed3d0ea7ac5e2
Jessica Sandoval <jessi.sandoval93@gmail.com>
2024-12-02 19:29:32 -0500

    class 12

---
+++ MODULO_3/Clase-12/challenge/challenge.js
@@ -0,0 +1,6 @@
+// Con axios consumir el servicio de JsonPlaceholder:  https://jsonplaceholder.typicode.com/users para crear un array de usuarios.
+// Añadir un nuevo usuario a la lista.
+// Eliminar un usuario de la lista
+// Actualizar datos de un usuario
+// Buscar usuario por id 
+// Buscar usuario por nombre
